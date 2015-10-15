b72e22463eedf0e05d83db37a37c47842a16422e
Eddyson Tan <eddysontan@eddysons-macbook-pro.local>
2015-10-15 10:53:05 +0700

    Adding Button and the control

--- gitExample/ViewController.m
+++ gitExample/ViewController.m
@@ -24,4 +24,30 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)goPencet:(id)sender{
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Notification!" message:@"Do you want some coffee?" preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertAction* yesButton = [UIAlertAction
+                                actionWithTitle:@"Yes, please"
+                                style:UIAlertActionStyleDefault
+                                handler:^(UIAlertAction * action)
+                                {
+                                    //Handel your yes please button action here
+                                    [alert dismissViewControllerAnimated:YES completion:nil];
+                                    
+                                }];
+    UIAlertAction* noButton = [UIAlertAction
+                               actionWithTitle:@"No, thanks"
+                               style:UIAlertActionStyleDefault
+                               handler:^(UIAlertAction * action)
+                               {
+                                   [alert dismissViewControllerAnimated:YES completion:nil];
+                                   
+                               }];
+    
+    [alert addAction:yesButton];
+    [alert addAction:noButton];
+    
+    [self presentViewController:alert animated:YES completion:nil];
+}
+
 @end
